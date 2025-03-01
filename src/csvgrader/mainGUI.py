a13659755664e3f993a168c8848bcdca99d3d9a3
Nathaniel Bowden <nbowden045@gmail.com>
2025-03-01 15:39:34 -0600

    Add critical error catching to mainGUI.py
    This will print out any errors that crash the
    program, although I think the Tk main loop also
    has error catching so that errors will print
    without crashing the program.

--- src/csvgrader/mainGUI.py
+++ src/csvgrader/mainGUI.py
@@ -1,4 +1,7 @@
 # Main.py
+
+import traceback
+
 from gui.mainWindow import MainWindow
 
 filepath = "/home/auttieb/Documents/college/TA/211LSP25/120251-PHYS-211-labs-1.csv"
@@ -8,4 +11,9 @@ submitpath = "/home/auttieb/Documents/college/TA/211LSP25/submissions/lab1/L3E"
 
 if __name__ == "__main__":
     mw = MainWindow()
-    mw.run()
+
+    try:
+        mw.run()
+    except Exception as e:
+        print("Critical Error")
+        print(traceback.print_exception(e))
